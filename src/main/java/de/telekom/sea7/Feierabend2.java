1c899c5f42a87afe163f7ed6b1e11516ef75afb1
sea18 <elisabeth.plucinski@telekomde>
2022-01-07 09:50:23 +0100

    Erstes-projekt start 07012022

---
+++ src/main/java/de/telekom/sea7/Feierabend2.java
@@ -0,0 +1,10 @@
+package de.telekom.sea7;
+
+public class Feierabend2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo Feierabend");
+	}
+
+}
---
+++ src/main/java/de/telekom/sea7/Mittagspause.java
@@ -0,0 +1,22 @@
+package de.telekom.sea7;
+
+public class Mittagspause {
+
+	public static void main(String[] args) {
+// TODO Auto-generated method stub
+		System.out.println("Gib irgendwas ein oder gib 'exit' ein");
+
+		String eingabe = System.console().readLine();
+		System.out.println(eingabe);
+
+		while (!eingabe.startsWith("exit")) {
+			System.out.println("Gib irgendwas ein oder gib 'exit' ein");
+
+			eingabe = System.console().readLine();
+			System.out.println(eingabe);
+
+		}
+
+	}
+
+}
